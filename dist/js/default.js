46fb4c82b0bf9dab32c6af6e39a31d7753facf2f
Khoerodin <khoerodin@live.com>
2017-03-02 14:03:58 +0700

    add highlight js

--- dist/js/default.js
+++ dist/js/default.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){});
+$(document).ready(function(){hljs.initHighlightingOnLoad(),$("pre:has(code)").css("padding","0"),$("pre:has(code)").css("border","0"),$("pre:has(code)").css("overflow","auto")});
