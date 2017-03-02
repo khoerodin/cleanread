ad659716558a7faf88a055b5b17a31345f9aef97
Khoerodin <khoerodin@live.com>
2017-03-02 14:05:56 +0700

    pre release single content template

--- src/js/default.js
+++ src/js/default.js
@@ -1,6 +1,10 @@
 // Default JavaScript Functions and Initiations
 $(document).ready(function() {
 
-  // Functions go here...
+  hljs.initHighlightingOnLoad();
+  $('pre:has(code)').css('padding', '0');
+  $('pre:has(code)').css('border', '0');
+
+  $('pre:has(code)').css('overflow', 'auto');
   
 }); // end document ready
